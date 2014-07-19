bf314eed487f6357131fb251c66392f2f03e7727
monochromegane <dev.kuro.obi@gmail.com>
2014-07-19 16:31:46 +0900

    Added a function that sets default value from tag info.

--- config.go
+++ config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"reflect"
+	"strconv"
 )
 
 func Parse(path string, config interface{}) error {
@@ -12,6 +14,7 @@ func Parse(path string, config interface{}) error {
 		return err
 	}
 
+	setDefaultValue(config)
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(config)
 	if err != nil {
@@ -19,4 +22,22 @@ func Parse(path string, config interface{}) error {
 	}
 
 	return nil
+}
+
+func setDefaultValue(config interface{}) {
+
+	v := reflect.ValueOf(config)
+	t := reflect.TypeOf(config)
+	for i := 0; i < t.Elem().NumField(); i++ {
+		f := t.Elem().FieldByIndex([]int{i})
+		def := f.Tag.Get("default")
+		value := v.Elem().FieldByName(f.Name)
+		switch f.Type.Kind() {
+		case reflect.Int:
+			iv, _ := strconv.Atoi(def)
+			value.SetInt(int64(iv))
+		case reflect.String:
+			value.SetString(def)
+		}
+	}
 }
--- config_test.go
+++ config_test.go
@@ -3,7 +3,7 @@ package config
 import "testing"
 
 type ExampleConfig struct {
-	Name string `json:name`
+	Name string `json:"name"`
 }
 
 func TestParse(t *testing.T) {
@@ -17,4 +17,22 @@ func TestParse(t *testing.T) {
 	if config.Name != "go-config" {
 		t.Errorf("It should be equal %s, but %s", "go-config", config.Name)
 	}
+}
+
+func TestSetDefalutValue(t *testing.T) {
+
+	var conf = struct {
+		String string `default:"default"`
+		Int    int    `default:"10"`
+	}{}
+
+	setDefaultValue(&conf)
+
+	if conf.String != "default" {
+		t.Errorf("Expected String value 'defalut', but %s.", conf.String)
+	}
+	if conf.Int != 10 {
+		t.Errorf("Expected String value 10, but %d.", conf.Int)
+	}
+
 }
